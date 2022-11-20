0e992749452f08e27183f185170abcc26a86cee3
Forgethat <victordinizd0006@gmail.com>
2022-11-20 19:59:54 -0300

    FixedNegativeCounter

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 var counter = document.getElementById('counter');
-var count = 0
+var count = 0;
 
 function increment() {
     count += + 1;
@@ -8,5 +8,9 @@ function increment() {
 function decrement() {
     count += - 1;
     counter.textContent = count;
+    if (count < 0) {
+        count = 0
+        counter.textContent = 0;
+    }
 }
 
